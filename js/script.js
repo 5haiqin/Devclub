433cc59c577582df2f07af980f1e90657ce1873d
Shaiqin <150903684+5haiqin@users.noreply.github.com>
2024-03-14 12:55:31 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/*===============Our Team====================*/
+var copy = document.querySelector(".members-slide").cloneNode(true);
+document.querySelector(".members").appendChild(copy);
+
+
+/*===============Gallery====================*/
+var copy = document.querySelector(".images-slide").cloneNode(true);
+document.querySelector(".images").appendChild(copy);
+
+
+/*===============FAQ====================*/
+let toggles = document.getElementsByClassName('toggle');
+let contentDiv = document.getElementsByClassName('content');
+let icons = document.getElementsByClassName('icon');
+for(let i=0; i<toggles.length; i++){
+    toggles[i].addEventListener('click', ()=>{
+        if( parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
+            contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+            toggles[i].style.color = "#58d999";
+            icons[i].classList.remove('fa-plus');
+            icons[i].classList.add('fa-minus');
+        }
+        else{
+            contentDiv[i].style.height = "0px";
+            toggles[i].style.color = "#111130";
+            icons[i].classList.remove('fa-minus');
+            icons[i].classList.add('fa-plus');
+        }
+        for(let j=0; j<contentDiv.length; j++){
+            if(j!==i){
+                contentDiv[j].style.height = "0px";
+                toggles[j].style.color = "#111130";
+                icons[j].classList.remove('fa-minus');
+                icons[j].classList.add('fa-plus');
+            }
+        }
+    });
+}
+
+
+/*===============Testimonels====================*/
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
